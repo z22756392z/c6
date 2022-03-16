00ecdb9517585691955a916342a8c04c19012583
z22756392z <79678055+z22756392z@users.noreply.github.com>
2022-03-16 20:46:20 +0800

    Update doWhile.c

--- test/doWhile.c
+++ test/doWhile.c
@@ -12,11 +12,5 @@ int main(){
 	} while(i < 10);
 	printf("s=%d i=%d\n",s,i);
 	
-	for (i = 0; i < 10; i = i + 1){
-		s = i + s;
-		i = i + 1;
-		printf("for: s=%d i=%d\n",s,i);
-	}
-	
 	return 0;
 }
